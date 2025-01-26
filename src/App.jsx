782875b251cebabd71f949e22f1db5bbc52c6216
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-01-26 15:32:12 -0300

    Criação das páginas

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header.jsx/header'
+import Main from './components/Main.jsx/main'
+import Footer from './components/Footer.jsx/footer'
+import './Global.Style.scss'
+
+
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}

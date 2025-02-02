9ad6b1dfe11673973f3bae37b5095cd7e8d00796
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-02-02 00:49:52 -0300

    header

--- src/components/Header.jsx/header.jsx
+++ src/components/Header.jsx/header.jsx
@@ -4,7 +4,27 @@
 export default function Header(){
     return(
         <>
-        Header
+        <h1>Lucas Vinicius</h1>
+        <section>
+            <a href="#">Sobre mim</a>
+            <a href="#">Projetos</a>
+            <a href="#">Conhecimentos</a>
+            <a href="#">Fale comigo</a>
+        </section>
+        <section>
+            <a href="#">
+                <img src="#" alt="" />
+            </a>
+            <a href="#">
+                <img src="#" alt="" />
+            </a>
+            <a href="#">
+                <img src="#" alt="" />
+            </a>
+            <a href="#">
+                <img src="#" alt="" />
+            </a>
+        </section>
         </>
     )
 }

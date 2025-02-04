a28d5fd3b5eb359facb231d3268e450666dc58f7
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-02-04 01:29:39 -0300

    header ok

--- src/components/Header.jsx/header.jsx
+++ src/components/Header.jsx/header.jsx
@@ -1,29 +1,34 @@
-
-
+import instagram from "../../assets/iconeInstagram.png"
+import github from "../../assets/github.png"
+import linkedin from "../../assets/linkedin.png"
+import twitter from "../../assets/twitter.png"
+import S from "../../components/Header.jsx/header.module.scss"
 
 export default function Header(){
     return(
         <>
-        <h1>Lucas Vinicius</h1>
-        <section>
-            <a href="#">Sobre mim</a>
-            <a href="#">Projetos</a>
-            <a href="#">Conhecimentos</a>
-            <a href="#">Fale comigo</a>
-        </section>
-        <section>
-            <a href="#">
-                <img src="#" alt="" />
-            </a>
-            <a href="#">
-                <img src="#" alt="" />
-            </a>
-            <a href="#">
-                <img src="#" alt="" />
-            </a>
-            <a href="#">
-                <img src="#" alt="" />
-            </a>
+        <section className={S.boxHeader} >
+            <h1 className={S.boxTitulo} >Lucas Vinicius</h1>
+            <section className={S.boxMenu} >
+                <a href="#">Sobre mim</a>
+                <a href="#">Projetos</a>
+                <a href="#">Conhecimentos</a>
+                <a href="#">Fale comigo</a>
+            </section>
+            <section className={S.boxImagem} >
+                <a href="#">
+                    <img src={instagram} alt="Logo instagram" />
+                </a>
+                <a href="#">
+                    <img src={github} alt="Logo do github" />
+                </a>
+                <a href="#">
+                    <img src={linkedin} alt="Logo do linkedin" />
+                </a>
+                <a href="#">
+                    <img src={twitter} alt="Logo do twitter" />
+                </a>
+            </section>
         </section>
         </>
     )

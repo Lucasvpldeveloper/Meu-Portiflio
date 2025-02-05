f5c3a7c86e4812a9c06a832a864f3aa89ecab146
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-02-05 00:42:08 -0300

    Inicio da Header

--- src/components/Main.jsx/main.jsx
+++ src/components/Main.jsx/main.jsx
@@ -1,10 +1,24 @@
-
-
+import M from "../Main.jsx/main.module.scss"
+import notebook from "../../assets/notebook.png"
 
 export default function Main(){
     return(
         <>
-        Main
+        <main className={M.boxMain} >
+            <section className={M.boxPrincipal} >
+                <section className={M.boxSecundaria} >
+                    <p>HELLO WORLD</p>
+                    <h1>Eu sou Lucas,</h1>
+                    <h3>Desenvolvedor Front-end.</h3>
+                    <button>FALE COMIGO</button>
+                </section>
+                <section className={M.boxBola}>
+                    <div>
+                        <img src={notebook} alt="" />
+                    </div>
+                </section>
+            </section>
+        </main>
         </>
     )
 }

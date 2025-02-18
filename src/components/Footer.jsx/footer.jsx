9ccd4242d4a1605738db47fda71ca2ea26c7b014
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-02-18 19:45:53 -0300

    Footer finalizado

--- src/components/Footer.jsx/footer.jsx
+++ src/components/Footer.jsx/footer.jsx
@@ -1,10 +1,30 @@
-
+import instagram from "../../assets/iconeInstagram.png"
+import github from "../../assets/github.png"
+import link from "../../assets/linkedin.png"
+import twitter from "../../assets/twitter.png"
+import F from "../Footer.jsx/footer.module.scss"
 
 
 export default function Footer(){
     return(
         <>
-        Footer
+        <footer className={F.boxFinal}>
+            <section className={F.boxCelular}>
+                <section className={F.boxDocumentos}>
+                    <h4>Lucas Vinicius</h4>
+                    <p>@2024 por Lucas Vinicius</p>
+                    <p>Todos os direitos reservados.</p>
+                </section>
+                <section className={F.boxIcones}>
+                    <ul>
+                        <li> <a href="#"> <img src={instagram} alt="Imagem logo instagram" /> </a> </li>
+                        <li> <a href="#"> <img src={github}alt="Imagem logo github" /> </a> </li>
+                        <li> <a href="#"> <img src={link} alt="Imagem logo linkedin" /> </a> </li>
+                        <li> <a href="#"> <img src={twitter} alt="Imagem logo do twitter" /> </a> </li>
+                    </ul>
+                </section>
+            </section>
+        </footer>
         </>
     )
 }

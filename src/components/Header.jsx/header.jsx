0d2f68911bbee074badf73526188e9a3dca5c9cf
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-02-22 01:31:43 -0300

    ultimos ajustes

--- src/components/Header.jsx/header.jsx
+++ src/components/Header.jsx/header.jsx
@@ -1,7 +1,6 @@
 import instagram from "../../assets/iconeInstagram.png"
 import github from "../../assets/github.png"
 import linkedin from "../../assets/linkedin.png"
-import twitter from "../../assets/twitter.png"
 import S from "../../components/Header.jsx/header.module.scss"
 
 export default function Header(){
@@ -10,23 +9,19 @@ export default function Header(){
         <section className={S.boxHeader} >
             <h1 className={S.boxTitulo} >Lucas Vinicius</h1>
             <section className={S.boxMenu} >
-                <a href="#">Sobre mim</a>
-                <a href="#">Projetos</a>
-                <a href="#">Conhecimentos</a>
-                <a href="#">Fale comigo</a>
+                <a href="#about" title="Sobre mim">Sobre mim</a>
+                <a href="https://github.com/Lucasvpldeveloper?tab=repositories" target="blank">Projetos</a>
+                <a href="https://wa.me/message/QGDJ52S6MKG6K1" target="blank">Fale comigo</a>
             </section>
             <section className={S.boxImagem} >
-                <a href="#">
+                <a href="https://www.instagram.com/lucasvpl?igsh=NmQzNDI2cnZ1OG90&utm_source=qr" target="blank">
                     <img src={instagram} alt="Logo instagram" />
                 </a>
-                <a href="#">
+                <a href="https://github.com/lucasvpldeveloper" target="blank">
                     <img src={github} alt="Logo do github" />
                 </a>
-                <a href="#">
+                <a href="https://www.linkedin.com/in/lucas-vpl-5b65aa31a/" target="blank">
                     <img src={linkedin} alt="Logo do linkedin" />
-                </a>
-                <a href="#">
-                    <img src={twitter} alt="Logo do twitter" />
                 </a>
             </section>
         </section>
--- src/components/Main.jsx/main.jsx
+++ src/components/Main.jsx/main.jsx
@@ -1,4 +1,4 @@
-import M from "../Main.jsx/main.module.scss"
+import S from "../Main.jsx/main.module.scss"
 import notebook from "../../assets/notebook.png"
 import vidro from "../../assets/vidro.png"
 import lucas from "../../assets/lucas.jpg"
@@ -6,28 +6,30 @@ import lucas from "../../assets/lucas.jpg"
 export default function Main(){
     return(
         <>
-        <main className={M.boxMain} >
-            <section className={M.boxPrincipal} >
-                <section className={M.boxIntroducao} >
+        <main className={S.boxMain} >
+            <section className={S.boxPrincipal} >
+                <section className={S.boxIntroducao} >
                     <p>HELLO WORLD</p>
                     <h1>Eu sou Lucas,</h1>
                     <h2>Desenvolvedor Front-end.</h2>
-                    <button>FALE COMIGO</button>
+                    <a href="https://wa.me/message/QGDJ52S6MKG6K1" target="blank"><button>FALE COMIGO</button></a>
                 </section>
-                <section className={M.boxBola}>
-                    <div className={M.boxIMG} >
-                        <img className={M.boxVidro} src={vidro} alt="" />
+                <section className={S.boxBola}>
+                    <div className={S.boxIMG} >
+                        <img className={S.boxVidro} src={vidro} alt="" />
                         <img src={notebook} alt="Imagem de um macbook" />
                     </div>
                 </section>
             </section>
-            <section className={M.boxSecundaria}>
-                <section className={M.boxInterna} >
-                    <section className={M.boxLucas} >
+            <section className={S.boxSecundaria}>
+                <section className={S.boxInterna} >
+                    <section className={S.boxLucas} >
                     <img src={lucas} alt="imagem lucas" />
                     </section>
-                    <section className={M.boxDescricao} >
-                        <h3>Sobre mim</h3>
+                    <section className={S.boxDescricao} >
+                        <div id="about" className={S.container} >
+                            <h3>Sobre mim</h3>
+                        </div>
                         <p>Me chamo Lucas Vinicius, graduando o curso de Analises e Desenvolvimento de Sistemas pela UNINASSAU e desenvolvedor Front-end. Desenvolvo páginas e sistemas web responsívos. Tenho uma trajetória de 4 anos como gerente de posto de gasolina, 1 ano no controle de estoque em uma cafeteria em Portugal e 2 anos como coordenador geral em um Colégio. O que me tornou um profissional dedicado e organizado. Hoje na área de desenvolvimento vivo o aprendizado continuo, Busco oportunidades no mercado como dev e, em um futuro próximo, viso me tornar um desenvolvedor Full Stack.  </p>
                     </section>
                 </section>

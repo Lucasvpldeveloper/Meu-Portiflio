548eac04a33519e9435b934ffc11db2cef099123
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-02-25 20:14:06 -0300

    correcao do bug sobre mim

--- src/components/Header.jsx/header.jsx
+++ src/components/Header.jsx/header.jsx
@@ -7,7 +7,7 @@ export default function Header(){
     return(
         <>
         <section className={S.boxHeader} >
-            <h1 className={S.boxTitulo} >Lucas Vinicius</h1>
+            <h1 className={S.boxTitulo}>Lucas Vinicius</h1>
             <section className={S.boxMenu} >
                 <a href="#about" title="Sobre mim">Sobre mim</a>
                 <a href="https://github.com/Lucasvpldeveloper?tab=repositories" target="blank">Projetos</a>
--- src/components/Main.jsx/main.jsx
+++ src/components/Main.jsx/main.jsx
@@ -21,13 +21,13 @@ export default function Main(){
                     </div>
                 </section>
             </section>
-            <section className={S.boxSecundaria}>
+            <section id="about" className={S.boxSecundaria}>
                 <section className={S.boxInterna} >
                     <section className={S.boxLucas} >
                     <img src={lucas} alt="imagem lucas" />
                     </section>
                     <section className={S.boxDescricao} >
-                        <div id="about" className={S.container} >
+                        <div className={S.container} >
                             <h3>Sobre mim</h3>
                         </div>
                         <p>Me chamo Lucas Vinicius, graduando o curso de Analises e Desenvolvimento de Sistemas pela UNINASSAU e desenvolvedor Front-end. Desenvolvo páginas e sistemas web responsívos. Tenho uma trajetória de 4 anos como gerente de posto de gasolina, 1 ano no controle de estoque em uma cafeteria em Portugal e 2 anos como coordenador geral em um Colégio. O que me tornou um profissional dedicado e organizado. Hoje na área de desenvolvimento vivo o aprendizado continuo, Busco oportunidades no mercado como dev e, em um futuro próximo, viso me tornar um desenvolvedor Full Stack.</p>
